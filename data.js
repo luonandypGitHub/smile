dd5cf8cd13c54a4b8547c0b1ad725f7cc6cad10a
luonandypGitHub <45893573+luonandypGitHub@users.noreply.github.com>
2019-03-04 16:26:45 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,42 +1,54 @@
 let coaches = [{
     name:"谢辉",
     ginder:1,
-    tel:"13488203185",
+    tel:13488203185,
     url:"https://pic1-s.styd.cn/o_1ch7q3gt61l5k1a107q2lvlsn4c.jpg?imageView2/1/w/120/h/120/interlace/1"
 },{
     name:"王楠",
     ginder:0,
-    tel:"13489203185",
+    tel:13489203185,
     url:"https://pic1-s.styd.cn/saas-no-avatar?imageView2/1/w/120/h/120/interlace/1"
 },{
     name:"贵生儿",
     ginder:1,
-    tel:"17889203185",
+    tel:17889203185,
     url:"https://pic1-s.styd.cn/saas-no-avatar?imageView2/1/w/120/h/120/interlace/1"
 },{
     name:"董亚平",
     ginder:1,
-    tel:"17889202213",
+    tel:17889202213,
     url:"https://pic1-s.styd.cn/saas-no-avatar?imageView2/1/w/120/h/120/interlace/1"
 }];
 let classes = [{
     name: "蛙泳",
     introduce:["蛙泳（breaststroke）是一种模仿青蛙游泳动作的游泳姿势，也是一种最古老的泳姿。蛙泳时，游泳者可以方便观察前方是否有障碍物，避免撞上障碍物。","蛙泳是竞技游泳姿势之一。人体俯卧水面，两臂在胸前对称直臂侧下屈划水，两腿对称屈伸蹬夹水，似青蛙游水。蛙泳较省力，易持久，实用价值大，常用于渔猎、泅渡、救护、水上搬运等，同时，也是游泳初学者的学习项目。"],
     time:"90分钟",
+    obj:"18岁以上",
+    phone:13430259980,
+    imglist:["http://f.hiphotos.baidu.com/baike/s%3D220/sign=a7bb7a658bd4b31cf43c93b9b7d6276f/9922720e0cf3d7ca969d5e50f21fbe096b63a9a9.jpg"],
     coach:[0,1,3]
 },{
     name: "蝶泳",
     introduce:["蝶泳是游泳项目之一，蝶泳技术是在蛙泳技术动作基础上演变而来的。当蛙泳技术发展到第二阶段时，在游泳比赛中，有些运动员采用两臂划水到大腿后提出水面，再从空中迁移的技术，从外形看，好像蝴蝶展翅飞舞，所以人们称它为“蝶泳”。","蝶泳在4种竞技游泳姿势中是最后发展起来的泳姿。由于它的腿部动作酷似海豚，所以又称为“海豚泳”。"],
     time:"90分钟",
+    obj:"18岁以上",
+    phone:13430259980,
+    imglist:["http://h.hiphotos.baidu.com/baike/s%3D220/sign=013d6420acaf2eddd0f14eebbd100102/574e9258d109b3deebb6fe4eccbf6c81800a4c8e.jpg"],
     coach:[0,1,2]
 },{
     name: "仰泳",
     introduce:["仰泳（backstroke），又称背泳，是一种人体仰卧在水中的游泳姿势。仰泳包括反蛙泳和反爬泳，因为脸面在水面上，呼吸很方便，但是游泳者看不到在往哪里游，容易错方向。仰泳是唯一运动员在水中开始的姿势，其它都是跳入水中。"],
     time:"90分钟",
+    obj:"18岁以上",
+    phone:13430259980,
+    imglist:["https://gss2.bdstatic.com/9fo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=3032bf10918fa0ec6bca6c5f47fe328b/2cf5e0fe9925bc310e827cbb5edf8db1cb137031.jpg"],
     coach:[0,1,2,3]
 },{
     name: "自由泳",
     introduce:["自由泳（freestyle）是竞技游泳比赛项目之一。严格的来说不是一种游泳姿势，它的竞赛规则几乎没有任何的限制，爬泳这种姿势结构合理，阻力小，速度均匀、快速，是最省力的一种游泳姿势。所以人们也把爬泳称为自由泳。1896年第一届奥运会自由泳被列为正式的比赛项目。20世纪50年代以前，游泳运动员都非常重视两腿打水的作用，一般都是两臂轮流划水1次就打腿6次。后来科研材料证明打腿的能量消耗比划臂大得多，而推动身体前进的动力主要来自臂部的划水动作。因此以臂为主的现代自由泳技术重视臂的划水动作和两臂的配合。"],
     time:"90分钟",
+    obj:"18岁以上",
+    phone:13430259980,
+    imglist:["https://gss2.bdstatic.com/-fo3dSag_xI4khGkpoWK1HF6hhy/baike/s%3D250/sign=399ff7b7b099a9013f355c332d940a58/0d338744ebf81a4c77a0c76adf2a6059242da686.jpg"],
     coach:[2,3]
 }];
